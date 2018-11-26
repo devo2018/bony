58785aeac8f945dbe2ec5cf664cc238dd61cdbdf
devo2018 <45319613+devo2018@users.noreply.github.com>
2018-11-26 23:36:53 +0200

    fix name

--- src/qt/bonystrings.cpp
+++ src/qt/bonystrings.cpp
@@ -235,10 +235,10 @@ QT_TRANSLATE_NOOP("bony-core", ""
 QT_TRANSLATE_NOOP("bony-core", ""
 "You must specify a masternodeprivkey in the configuration. Please see "
 "documentation for help."),
-QT_TRANSLATE_NOOP("bony-core", "(51472 could be used only on mainnet)"),
+QT_TRANSLATE_NOOP("bony-core", "(9530 could be used only on mainnet)"),
 QT_TRANSLATE_NOOP("bony-core", "(default: %s)"),
 QT_TRANSLATE_NOOP("bony-core", "(default: 1)"),
-QT_TRANSLATE_NOOP("bony-core", "(must be 51472 for mainnet)"),
+QT_TRANSLATE_NOOP("bony-core", "(must be 9530 for mainnet)"),
 QT_TRANSLATE_NOOP("bony-core", "<category> can be:"),
 QT_TRANSLATE_NOOP("bony-core", "Accept command line and JSON-RPC commands"),
 QT_TRANSLATE_NOOP("bony-core", "Accept connections from outside (default: 1 if no -proxy or -connect)"),
--- src/qt/locale/bony_sv.ts
+++ src/qt/locale/bony_sv.ts
@@ -5062,8 +5062,8 @@ Minta antingen högre valörer (så att färre inputs behövs) eller spendera mi
         <translation>Du måste specifiera en masternodeprivkey i konfigurationen. Var vänlig se dokumentationen för hjälp.</translation>
     </message>
     <message>
-        <source>(51472 could be used only on mainnet)</source>
-        <translation>(51472 kunde endast användas på mainnet)</translation>
+        <source>(9530 could be used only on mainnet)</source>
+        <translation>(9530 kunde endast användas på mainnet)</translation>
     </message>
     <message>
         <source>(default: %s)</source>
@@ -5074,8 +5074,8 @@ Minta antingen högre valörer (så att färre inputs behövs) eller spendera mi
         <translation>(standard: 1)</translation>
     </message>
     <message>
-        <source>(must be 51472 for mainnet)</source>
-        <translation>(måste vara 51472 för mainnet)</translation>
+        <source>(must be 9530 for mainnet)</source>
+        <translation>(måste vara 9530 för mainnet)</translation>
     </message>
     <message>
         <source>Accept command line and JSON-RPC commands</source>
